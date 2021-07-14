2edb467395601ed64d80e26a0684f7880e3d052f
David Brooks <junglebarry@users.noreply.github.com>
2021-07-14 20:26:47 +0100

    chore: linting (#3)
    * chore: install prettier
    * chore: add npm scripts for linting and formatting
    * chore: add linting step to build
    * chore: apply formatting
    * chore: install eslint and @typescript-eslint
    * chore: install eslint confiuguration
    * chore: eslint-ignore

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,16 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface TalisCdkConstructsProps {
   // Define construct properties here
 }
 
 export class TalisCdkConstructs extends cdk.Construct {
-
-  constructor(scope: cdk.Construct, id: string, props: TalisCdkConstructsProps = {}) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: TalisCdkConstructsProps = {}
+  ) {
     super(scope, id);
 
     // Define construct contents here
