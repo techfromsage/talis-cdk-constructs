1a7469e93329530f16e19222b37918d97444cada
David Brooks <david.brooks@talis.com>
2021-07-14 20:16:45 +0100

    chore: apply formatting

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,15 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
 
 export interface TalisCdkConstructsProps {
   // Define construct properties here
 }
 
 export class TalisCdkConstructs extends cdk.Construct {
-
-  constructor(scope: cdk.Construct, id: string, props: TalisCdkConstructsProps = {}) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: TalisCdkConstructsProps = {}
+  ) {
     super(scope, id);
 
     // Define construct contents here
