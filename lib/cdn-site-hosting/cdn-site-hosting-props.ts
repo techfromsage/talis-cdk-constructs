1499f8b8fc2e483051f19165698031070d123311
David Brooks <david.brooks@talis.com>
2021-07-28 11:51:39 +0100

    feat: add ability to specify deployment sources with granular control

---
+++ lib/cdn-site-hosting/cdn-site-hosting-props.ts
@@ -0,0 +1,18 @@
+import * as cdk from "@aws-cdk/core";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
+
+export interface SourcesWithDeploymentOptions {
+  sources: s3deploy.ISource[];
+  distributionPathsToInvalidate?: string[];
+  cacheControl?: s3deploy.CacheControl[];
+}
+
+export interface CommonCdnSiteHostingProps {
+  domainName: string;
+  removalPolicy: cdk.RemovalPolicy;
+  siteSubDomain: string;
+  sources?: s3deploy.ISource[];
+  sourcesWithDeploymentOptions?: SourcesWithDeploymentOptions[];
+  websiteErrorDocument: string;
+  websiteIndexDocument: string;
+}
