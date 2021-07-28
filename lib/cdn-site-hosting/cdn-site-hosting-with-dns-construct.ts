a193b129027fca78d74f668e9b14998992322b11
David Brooks <david.brooks@talis.com>
2021-07-28 11:51:39 +0100

    feat: add ability to specify deployment sources with granular control

--- lib/cdn-site-hosting/cdn-site-hosting-with-dns-construct.ts
+++ lib/cdn-site-hosting/cdn-site-hosting-with-dns-construct.ts
@@ -2,18 +2,11 @@ import * as cdk from "@aws-cdk/core";
 import * as certificatemanager from "@aws-cdk/aws-certificatemanager";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as targets from "@aws-cdk/aws-route53-targets/lib";
-import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 import { CdnSiteHostingConstruct } from "./cdn-site-hosting-construct";
 import { getSiteDomain } from "./utils";
+import { CommonCdnSiteHostingProps } from "./cdn-site-hosting-props";
 
-export interface CdnSiteHostingWithDnsConstructProps {
-  domainName: string;
-  removalPolicy: cdk.RemovalPolicy;
-  siteSubDomain: string;
-  sources: s3deploy.ISource[];
-  websiteErrorDocument: string;
-  websiteIndexDocument: string;
-}
+export type CdnSiteHostingWithDnsConstructProps = CommonCdnSiteHostingProps;
 
 /**
  * Establishes infrastructure to host a static-site or single-page-application in S3 via CloudFront,
