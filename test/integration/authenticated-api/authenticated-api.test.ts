a0c52822163df1c80c2bfceaf84ca44ba017d255
MalcyL <malcolm@landonsonline.me.uk>
2021-12-02 09:53:46 +0000

    feat: wip

--- test/integration/authenticated-api/authenticated-api.test.ts
+++ test/integration/authenticated-api/authenticated-api.test.ts
@@ -1,5 +1,32 @@
+const AWS = require('aws-sdk');
+
+const api = new AWS.ApiGatewayV2();
+
 describe("AuthenticatedApi", () => {
-  test("test", () => {
-    expect(false).toBe(true);
+  
+  let apiGatewayId: String|null = null;
+
+  async function findApiGatewayId(nextToken: String|null = null) : Promise<String> {
+    const response = await api.getApis({ NextToken: nextToken }).promise()
+    for (const item of response.Items) {
+      if (item.Name === `${process.env.AWS_PREFIX}simple-authenticated-api`) {
+        return item.ApiId;
+      }
+    }
+
+    console.log(`Response NextToken : ${JSON.stringify(response.NextToken)}`);
+    if (response.NextToken) {
+      return await findApiGatewayId(response.NextToken);
+    }
+
+    throw Error('ApiGateway not found');
+  }
+
+  beforeAll(async () => {
+    apiGatewayId = await findApiGatewayId();
+  });
+
+  test("returns 200 for unauthenticated route", async () => {
+    expect(apiGatewayId).toBe('bob');
   });
 });
