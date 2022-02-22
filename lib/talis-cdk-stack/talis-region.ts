4c43d082a6ba4580ba5340034eae26d48c32f0e5
Shaun Parsons <github@shaunparsons.co.uk>
2022-02-22 15:34:11 +0000

    fix: add local region and ondemand deployment env

--- lib/talis-cdk-stack/talis-region.ts
+++ lib/talis-cdk-stack/talis-region.ts
@@ -1,4 +1,11 @@
 export enum TalisRegion {
   CANADA = "ca-central-1",
   EU = "eu-west-1",
+  LOCAL = "local",
+}
+  
+export enum TalisShortRegion {
+  CANADA = "ca",
+  EU = "eu",
+  LOCAL = "local",
 }
