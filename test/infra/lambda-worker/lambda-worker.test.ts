c0d6a21c08d51c62f46f37097b923a629416d780
MalcyL <malcolm@landonsonline.me.uk>
2022-09-01 08:43:15 +0100

    fix: - LambdaWorker - Age alarm on main queue not dlq

--- test/infra/lambda-worker/lambda-worker.test.ts
+++ test/infra/lambda-worker/lambda-worker.test.ts
@@ -619,7 +619,7 @@ describe("LambdaWorker", () => {
                 Name: "QueueName",
                 Value: {
                   "Fn::GetAtt": [
-                    "MyTestLambdaWorkerMyTestLambdaWorkerdlq27BBFD95",
+                    "MyTestLambdaWorkerMyTestLambdaWorkerqueue01D6E79E",
                     "QueueName",
                   ],
                 },
