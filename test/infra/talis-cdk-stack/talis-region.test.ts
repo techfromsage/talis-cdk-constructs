914b85ec77e319bcf9f963c2458b53c6b0ffaf34
Juan Casimiro <juan.casimiro@sagepub.co.uk>
2023-03-01 11:40:44 +0000

    feat: added new tests

--- test/infra/talis-cdk-stack/talis-region.test.ts
+++ test/infra/talis-cdk-stack/talis-region.test.ts
@@ -1,4 +1,8 @@
-import { TalisRegion } from "../../../lib";
+import {
+  TalisRegion,
+  TalisShortRegion,
+  getTalisShortRegionFromTalisRegionName,
+} from "../../../lib";
 
 describe("Talis Region", () => {
   test("Defines Canada region as ca-central-1", () => {
@@ -6,5 +10,30 @@ describe("Talis Region", () => {
   });
   test("Defines EU region as eu-west-1", () => {
     expect(TalisRegion.EU).toBe("eu-west-1");
+  });
+});
+
+describe("Talis Short Region", () => {
+  test("Defines Canada region as ca", () => {
+    expect(TalisShortRegion.CANADA).toBe("ca");
+  });
+  test("Defines EU region as eu", () => {
+    expect(TalisShortRegion.EU).toBe("eu");
+  });
+});
+
+describe("getTalisShortRegionFromTalisRegionName", () => {
+  test("Returns correct short region for Canada", () => {
+    expect(getTalisShortRegionFromTalisRegionName(TalisRegion.CANADA)).toBe(
+      TalisShortRegion.CANADA
+    );
+  });
+  test("Returns correct short region for EU", () => {
+    expect(getTalisShortRegionFromTalisRegionName(TalisRegion.EU)).toBe(
+      TalisShortRegion.EU
+    );
+  });
+  test("Returns undefined if the env region is not defined", () => {
+    expect(getTalisShortRegionFromTalisRegionName("foo")).toBeUndefined();
   });
 });
