b7bc79b978ffe56f530b324873b571d0f5dc309c
MalcyL <malcolm@landonsonline.me.uk>
2023-09-15 13:21:36 +0100

    Temp delete examples

--- examples/simple-talis-cdk-stack/lib/simple-talis-cdk-stack.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from "@aws-cdk/core";
-
-import { TalisCdkStack, TalisCdkStackProps } from "../../../lib";
-
-export class SimpleTalisCdkStack extends TalisCdkStack {
-  constructor(scope: cdk.Construct, id: string, props: TalisCdkStackProps) {
-    super(scope, id, props);
-  }
-}
