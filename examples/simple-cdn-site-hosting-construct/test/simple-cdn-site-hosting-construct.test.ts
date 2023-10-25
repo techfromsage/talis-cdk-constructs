531bb54f88d330beddac2c583b148a3be80b057f
MalcyL <malcolm@landonsonline.me.uk>
2023-10-25 08:36:37 +0100

    PLT-62 Add simple cdn site hosting construct example

---
+++ examples/simple-cdn-site-hosting-construct/test/simple-cdn-site-hosting-construct.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleCdnSiteHostingConstruct from '../lib/simple-cdn-site-hosting-construct-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple-cdn-site-hosting-construct-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleCdnSiteHostingConstruct.SimpleCdnSiteHostingConstructStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
